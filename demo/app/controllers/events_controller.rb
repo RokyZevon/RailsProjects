4a336a01d257a27220265940411697266c0b4140
RokyZevon <rk_zrj@hotmail.com>
2016-08-06 17:23:14 +0800

    ihower.tw/rails4/basic.html

--- demo/app/controllers/events_controller.rb
+++ demo/app/controllers/events_controller.rb
@@ -1,25 +1,62 @@
 class EventsController < ApplicationController
 
-	def index
-	end
+	before_action :set_event, :only => [ :show, :edit, :update, :destroy ]
 
-	def create
+	def index
+		@events = Event.page( params[ :page ] ).per( 5 )
 	end
 
 	def new
+		@event = Event.new
+	end
+
+	def create
+		@event = Event.new( event_params )
+		if @event.save
+			redirect_to :action => :index
+
+			flash[ :notice ] = "创建成功"
+		else
+			render :action => :new
+		end
+	end
+
+	def show
+		@page_title = @event.name
 	end
 
 	def edit
 	end
-	
-	def show
-	end
 
 	def update
+		if @event.update( event_params )
+			redirect_to :action => :show, :id => @event
+
+			flash[ :notice ] = "修改成功"
+		else
+			render :action => :edit
+		end
 	end
 
 	def destroy
+		@event.destroy
+
+		redirect_to :action => :index
+
+		flash[ :alter ] = "删除成功"
 	end
 
+
+	private
+	
+	# 白名单
+	def event_params
+		params.require( :event ).permit( :name, :description )
+	end
+
+	# before_action
+	def set_event
+		@event = Event.find( params[ :id ] )
+	end
 
 end
--- demo/config/routes.rb
+++ demo/config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
 
-	resources :events
+#	resources :events
 
 	root 'welcome#index'
 
