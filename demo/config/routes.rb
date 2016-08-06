64fe07a6c11e7a0a9fe3552deb22ae554de4d69a
RokyZevon <rk_zrj@hotmail.com>
2016-08-06 15:32:23 +0800

    model events && 外卡路由

--- demo/config/routes.rb
+++ demo/config/routes.rb
@@ -8,7 +8,9 @@ Rails.application.routes.draw do
 
 	get 'welcome/say_hello' => 'welcome#say'
 
-
+	# 外卡路由
+	# 它會將/foo/bar這樣的網址自動對應到Controller foo的bar Action、如果是/foo這樣的網址，則預設對應到index action。
+	match ':controller( /:action( /:id( .:format ) ) )', :via => :all
 
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
